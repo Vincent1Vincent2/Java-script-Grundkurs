2cbe044520b51704d63029d27fabaecee31d6e9a
Vincent <Vincent.ahlin99@gmail.com>
2023-10-27 11:10:39 +0200

    removed code i didnt use and change font sizes

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-// console.log("Vincent Ahlin");
+const worldTxt = document.getElementById("worldTxt");
 
-// let awnser = prompt("Vad är ditt favorit djur?");
-// console.log(awnser);
-// alert(awnser);
-
-// let numberTestOne = prompt("Skriv in en siffra");
-// console.log(numberTestOne);
-// let numberTestTwo = prompt("Skriv in en till siffra");
-// console.log(numberTestTwo);
-
-// let awnser = Number(numberTestOne) + Number(numberTestTwo);
-
-// alert(awnser);
-
-// const favColor = prompt("Vad är din favoritfärg?");
-
-// function favColorButton() {
-//   onclick = console.log(favColor);
-// }
-
-// const firstName = prompt("Förnamn");
-// const lastName = prompt("Efternamn");
-
-// function fullName() {
-//   alert(firstName + " " + lastName);
-// }
-
-// fullName();
-
-function changetxt() {
-  document.getElementById("heroTitle").innerText = "This is Vincents World";
-}
+worldTxt.addEventListener("click", function () {
+  heroTitle.innerText = "Vincents World";
+});
 
 function colorChange() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -39,45 +11,7 @@ function colorChange() {
   color.innerHTML = "#" + randomColor;
 }
 
-// function colorChangeTwo() {
-//   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-//   document.getElementById("colorBoxTwo").style.backgroundColor =
-//     "#" + randomColor;
-//   color = "#" + randomColor;
-// }
-
-// const user = {
-//   username: "",
-//   password: "jibos",
-//   age: 69,
-//   isAdmin: true,
-// };
-
-// console.log(user);
-
-// const userInfo = [
-//   prompt("Enter username"),
-//   prompt("Where do you live?"),
-//   prompt("Where were you born?"),
-//   prompt("How old are you?"),
-//   prompt("Whats your favorite food?"),
-// ];
-
-// function displayUserInfo() {
-//   console.log(userInfo.reverse());
-// }
-
-// const mathNumber = prompt("Write a number");
-// const mathOperator = prompt("Enter a operator (+ - / *)");
-// const mathNumberTwo = prompt("Write another number");
-
-// function mathNumberCombiner(mathNumber, mathNumberTwo, mathNumberCombiner) {
-//   return eval(`${Number(mathNumber)} ${mathOperator} ${Number(mathNumberTwo)}`);
-// }
-
-// const result = mathNumberCombiner(mathNumber, mathNumberTwo, mathOperator);
-// alert(`The result is ${result}`);
-
+//Darkmode
 function calculateSettingAsThemeString({
   localStorageTheme,
   systemSettingDark,
@@ -126,30 +60,7 @@ button.addEventListener("click", (event) => {
   currentThemeSetting = newTheme;
 });
 
-// function boxSize() {
-//   document.querySelector(".box").style.height = "200px";
-//   document.querySelector(".box").style.width = "200px";
-// }
-
-// function boxSizeChange() {
-//   const boxSize = document.querySelector(".box");
-//   if (boxSize.style.height == "") boxSize.style.height = "200px";
-//   else boxSize.style.height = "";
-
-//   if (boxSize.style.width == "") boxSize.style.width = "200px";
-//   else boxSize.style.width = "";
-// }
-
-// function dropShadowHover() {
-//   const dropShadow = document.querySelector(".box");
-//   dropShadow.style.boxShadow = "10px 20px 30px black";
-// }
-
-// function dropShadowLeave() {
-//   const dropShadow = document.querySelector(".box");
-//   dropShadow.style.boxShadow = "";
-// }
-
+//Hamburger
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
 const body = document.querySelector("body");
@@ -159,3 +70,14 @@ hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("active");
   body.classList.toggle("active");
 });
+
+const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function () {
+//   const addBrand = prompt(
+//     "Write a car brand name but not VOLVO, SAAB, PORSCHE, BUGGATI"
+//   );
+//   carBrands.pop();
+//   carBrands.push(addBrand);
+//   console.log(carBrands);
+// });
